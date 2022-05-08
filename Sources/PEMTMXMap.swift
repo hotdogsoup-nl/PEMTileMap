f94284c6623097506ff720de475b34cb56a55233
Martin van Spanje <martin@p-edge.nl>
2022-05-08 12:08:16 +0200

    Code cleaning.

--- Sources/PEMTMXMap.swift
+++ Sources/PEMTMXMap.swift
@@ -71,37 +71,7 @@ class PEMTMXMap : SKNode, XMLParserDelegate {
     internal var currentFirstGid = UInt(0)
     internal var currentMapElement = MapElements.None
     
-//    var parentElement = PEMTMXPropertyType.None
-//    var parentGID = Int(0)
-//    var orientation : PEMTMXOrientationStyle?
-//
-//    private (set) var minZPositioning = CGFloat(0)
-//    private (set) var maxZPositioning = CGFloat(0)
-//
-//    var resourcePath : String?
-//    var tileProperties : Dictionary<String, Any>? // xxx
-//    var properties : Dictionary<String, Any>? // xxx
-//    var layers : Array<PEMTMXLayerInfo>?
-//    var imageLayers : Array<PEMTMXImageLayer>?
-//    var objectGroups : Array<PEMTMXObjectGroup>?
-//    var gidData : Array<Any>? // xxx
-//    var cullNodes = false
-//
-//    private var currentString : String?
-//    private var storingCharacters = false
-//    private var currentFirstGID = Int(0)
-//    private var layerAttributes = PEMTMXLayerAttribute.None
-//    private var lastVisibleRect = CGRect.zero
-//    private var zOrderCount = Int(1)
-    
     override init() {
-//        currentString = String.init()
-//        tilesets = Array.init()
-//        tileProperties = Dictionary.init()
-//        properties = Dictionary.init()
-//        layers = Array.init()
-//        imageLayers = Array.init()
-//        objectGroups = Array.init()
         super.init()
     }
     
@@ -127,8 +97,6 @@ class PEMTMXMap : SKNode, XMLParserDelegate {
     init?(mapName : String, baseZPosition : CGFloat = 0, zPositionLayerDelta : CGFloat = 20) {
         super.init()
 
-        
-  
         if let path = bundleURLForResource(mapName) {
             if let parser = XMLParser(contentsOf: path) {
                 parser.delegate = self
@@ -152,76 +120,9 @@ class PEMTMXMap : SKNode, XMLParserDelegate {
             addChild(backgroundColorNode!)
         }
         
+        #if DEBUG
         print (self)
-        
-//
-//                if (baseZPosition < (baseZPosition + zOrderModifier * CGFloat(zOrderCount + 1))) {
-//                    minZPositioning = baseZPosition;
-//                    maxZPositioning = baseZPosition + zOrderModifier * CGFloat(zOrderCount + 1);
-//                } else {
-//                    maxZPositioning = baseZPosition;
-//                    minZPositioning = baseZPosition + zOrderModifier * CGFloat(zOrderCount + 1);
-//                }
-//
-//                for layerInfo in layers! {
-//                    if layerInfo.visible {
-//                        let child = PEMTMXLayer.init(tilesets: tilesets!, layerInfo: layerInfo, mapInfo: self)
-//                        child?.zPosition = baseZPosition + CGFloat(zOrderCount - layerInfo.zOrderCount) * zOrderModifier
-//                        addChild(child!)
-//                    }
-//                }
-//
-//                for objectGroup in objectGroups! {
-//                    for obj in objectGroup.objects {
-//                        if let num = Int(obj["gid"] ?? "0") {
-//                            if num != 0 {
-//                                if let tileset = tilesetInfoForGid(num) {
-//                                    let x = CGFloat(Double(obj["x"] ?? "0")!)
-//                                    let y = CGFloat(Double(obj["y"] ?? "0")!)
-//                                    var pt : CGPoint
-//
-//                                    if (orientation == .Isometric) {
-//                                        //these appear to be incorrect for iso maps when used for tile objects! Unsure why the math is different between objects and regular tiles.
-//                                        let coords = screenCoordToPosition(CGPoint(x: x, y: y))
-//                                        pt = CGPoint(x: (tileSize.width / 2.0) * (tileSize.width + coords.x - coords.y - 1), y: (tileSize.height / 2.0) * (((tileSize.height * 2) - coords.x - coords.y) - 2))
-//
-//                                        //    iso zPositioning may not work as expected for maps with irregular tile sizes.  For larger tiles (i.e. a box in front of some floor
-//                                        //    tiles) We would need each layer to have their tiles ordered lower at the bottom coords and higher at the top coords WITHIN THE LAYER, in
-//                                        //    addition to the layers being offset as described below. this could potentially be a lot larger than 20 as a default and may take some
-//                                        //    thinking to fix.
-//
-//                                    } else {
-//                                        pt = CGPoint(x: (x + tileSize.width / 2.0), y: (y + tileSize.height / 2.0))
-//                                    }
-//
-//                                    let texture = tileset.textureForGid(num - Int(tileset.firstGid) + 1)
-//                                    let sprite = SKSpriteNode(texture: texture)
-//                                    sprite.position = pt
-//                                    sprite.zPosition =
-//                                    baseZPosition + CGFloat(zOrderCount - objectGroup.zOrderCount) * zOrderModifier
-//
-//                                    addChild(sprite)
-//
-//                                    // This needs to be optimized into tilemap layers like our regular layers above for performance reasons.
-//                                                        // this could be problematic...  what if a single object group had a bunch of tiles from different tilemaps?  Would this cause zOrder problems if we're adding them all to tilemap layers?
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                for imageLayer in imageLayers! {
-//                    let image = SKSpriteNode(imageNamed: imageLayer.imageSource!)
-//                    image.position = CGPoint(x: image.size.width / 2.0, y: image.size.height / 2.0)
-//                    image.zPosition = baseZPosition + CGFloat(zOrderCount - imageLayer.zOrderCount) * zOrderModifier
-//                    addChild(image)
-//
-//                    //the positioning is off here, seems to be bottom-left instead of top-left.  Might be off on the rest of the sprites too...?
-//                }
-//
-//                cullNodes = true // xxx in the original this is set to 1 (while it is a boolean)
-//            }
-//        }
+        #endif
     }
     
     internal func getAttributes(_ attributes : Dictionary<String, String>) {
@@ -296,30 +197,6 @@ class PEMTMXMap : SKNode, XMLParserDelegate {
     private func mapSizePoints() -> CGSize {
         return CGSize(width: mapSize.width * tileSize.width, height: mapSize.height * tileSize.height)
     }
-
-//    func layerNamed(_ name : String) -> PEMTMXLayer? {
-//        return nil
-//    }
-//
-//    func groupNamed(_ name : String) -> PEMTMXObjectGroup? {
-//        return nil
-//    }
-//
-//    func tilesetInfoForGid(_ gID : Int) -> PEMTMXTilesetInfo? {
-//        return nil
-//    }
-//
-//    func propertiesForGid(_ gID : Int) -> Dictionary<String, Any>? { // xxx
-//        return nil
-//    }
-//
-//    func screenCoordToPosition(_ screenCoord : CGPoint) -> CGPoint {
-//        var returnValue = CGPoint.zero
-//        returnValue.x = screenCoord.x / tileSize.width
-//        returnValue.y = screenCoord.y / tileSize.height
-//
-//        return returnValue
-//    }
     
     #if DEBUG
     override var description: String {
