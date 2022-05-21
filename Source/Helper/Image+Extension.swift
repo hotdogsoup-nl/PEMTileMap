26a42e5e670717339831b2dabb5a16c8520fab28
Martin van Spanje <martin@p-edge.nl>
2022-05-21 17:48:14 +0200

    Add Image+Extension.swift.

---
+++ Source/Helper/Image+Extension.swift
@@ -0,0 +1,39 @@
+#if os(macOS)
+
+import AppKit
+import SpriteKit
+
+// this should work on an SKTexture.cgImage but unfortunately it doesn't, so we use NSImage / UIImage
+
+extension NSImage {
+    func remove(color: NSColor, tolerance: CGFloat = 4) -> NSImage {
+        if let ciColor = CIColor(color: color) {
+            let maskComponents: [CGFloat] = [ciColor.red, ciColor.green, ciColor.blue].flatMap { value in
+                [(value * 255) - tolerance, (value * 255) + tolerance]
+            }
+            
+            guard let masked = cgImage(forProposedRect: nil, context: nil, hints: nil)?.copy(maskingColorComponents: maskComponents) else { return self }
+            return NSImage(cgImage: masked, size: size)
+        }
+
+        return self
+    }
+}
+
+#else
+
+import UIKit
+
+extension UIImage {
+    func remove(color: UIColor, tolerance: CGFloat = 4) -> UIImage {
+        let ciColor = CIColor(color: color)
+        let maskComponents: [CGFloat] = [ciColor.red, ciColor.green, ciColor.blue].flatMap { value in
+            [(value * 255) - tolerance, (value * 255) + tolerance]
+        }
+        
+        guard let masked = cgImage?.copy(maskingColorComponents: maskComponents) else { return self }
+        return UIImage(cgImage: masked)
+    }
+}
+
+#endif
