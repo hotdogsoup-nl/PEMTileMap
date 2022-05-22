4d4b7a86af720fd0a498f977e4bd10ff85235e42
Martin van Spanje <martin@p-edge.nl>
2022-05-22 15:42:24 +0200

    Add CGPoint extension.

--- Sources/Helper/CG+Extensions.swift
+++ Sources/Helper/CG+Extensions.swift
@@ -1,8 +1,30 @@
-import CoreGraphics
+import SpriteKit
 
 extension CGFloat {
     internal func radians() -> CGFloat {
         return CGFloat(Double.pi) * (self / 180)
+    }
+}
+
+extension CGPoint {
+    func add(_ point : CGPoint) -> CGPoint {
+        return CGPoint(x: x + point.x, y: y + point.y)
+    }
+
+    func subtract(_ point : CGPoint) -> CGPoint {
+        return CGPoint(x: x - point.x, y: y - point.y)
+    }
+
+    func multiplyScalar(_ value : CGFloat) -> CGPoint {
+        return CGPointFromGLKVector2(GLKVector2MultiplyScalar(GLKVector2FromCGPoint(self), Float(value)))
+    }
+
+    private func CGPointFromGLKVector2(_ vector : GLKVector2) -> CGPoint {
+        return CGPoint(x: CGFloat(vector.x), y: CGFloat(vector.y))
+    }
+
+    private func GLKVector2FromCGPoint(_ point : CGPoint ) -> GLKVector2 {
+        return GLKVector2Make(Float(point.x), Float(point.y))
     }
 }
 
